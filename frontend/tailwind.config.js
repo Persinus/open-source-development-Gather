a27a379c8f71502745b4a81c94e6fa4024cf2328
Persinus <nguyenmanh2004devgame@gmail.com>
2025-08-17 16:38:19 +0700

    Update UI: tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,31 +1,74 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-        backgroundImage: {
-            "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-            "gradient-conic":
-            "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-            'rainbow': "linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet)",
-            'rainbow-less': "linear-gradient(45deg, red, blue, indigo)",
-            'rainbow-less-hover': "linear-gradient(45deg, #ff4d4d, #4d4dff, #4d4dff)",
-            'rainbow-less-disabled': "linear-gradient(45deg, darkred, darkblue, darkindigo)",
-        },
-      colors: {
-        primary: "#282D4E",
-        secondary: "#323A64",
-        "light-secondary": '#545C8E',
-        darkblue: "#2a3154",
-        quaternary: "#06d6a0",
-        quaternaryhover: "#5FE5C2",
-        button: "#202540",
-        'light-gray': "#464B67",
-      },
-    },
+  	extend: {
+  		backgroundImage: {
+  			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+  			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  			rainbow: 'linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet)',
+  			'rainbow-less': 'linear-gradient(45deg, red, blue, indigo)',
+  			'rainbow-less-hover': 'linear-gradient(45deg, #ff4d4d, #4d4dff, #4d4dff)',
+  			'rainbow-less-disabled': 'linear-gradient(45deg, darkred, darkblue, darkindigo)'
+  		},
+  		colors: {
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			'light-secondary': '#545C8E',
+  			darkblue: '#2a3154',
+  			quaternary: '#06d6a0',
+  			quaternaryhover: '#5FE5C2',
+  			button: '#202540',
+  			'light-gray': '#464B67',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
