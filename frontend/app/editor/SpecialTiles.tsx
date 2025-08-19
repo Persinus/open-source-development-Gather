63d224a8b522223cb6dc705d79bdd16c45fa9067
Persinus <nguyenmanh2004devgame@gmail.com>
2025-08-19 11:42:56 +0700

    feat(editor): thêm/điều chỉnh SpecialTiles

--- frontend/app/editor/SpecialTiles.tsx
+++ frontend/app/editor/SpecialTiles.tsx
@@ -9,37 +9,39 @@ type SpecialTilesProps = {
 }
 
 const SpecialTiles:React.FC<SpecialTilesProps> = ({ specialTile, selectSpecialTile }) => {
-
     return (
-        <div className='w-full flex flex-col items-center'>
+        <div className='w-full flex flex-col items-center gap-3 p-3 text-white'>
             <SpecialTileItem 
                 iconColor='yellow' 
-                title='Private Area' 
-                description='Specify an area where players can talk privately.' 
+                title='Khu vực riêng tư' 
+                description='Tạo khu vực để người chơi nói chuyện riêng.' 
                 selected={specialTile === 'Private Area'} 
                 onClick={() => selectSpecialTile('Private Area')}>
                 <Desk className='w-12 h-12'/>
             </SpecialTileItem>
+
             <SpecialTileItem 
                 iconColor='red' 
-                title='Impassable' 
-                description='Specify tiles that cannot be walked on.' 
+                title='Không thể đi qua' 
+                description='Đánh dấu các ô mà người chơi không thể bước vào.' 
                 selected={specialTile === 'Impassable'} 
                 onClick={() => selectSpecialTile('Impassable')}>
                 <Placeholder className='w-12 h-12'/>
             </SpecialTileItem>
+
             <SpecialTileItem 
                 iconColor='blue' 
-                title='Teleport' 
-                description='Set up a one-way teleporter between tiles.' 
+                title='Dịch chuyển' 
+                description='Tạo điểm dịch chuyển một chiều giữa các ô.' 
                 selected={specialTile === 'Teleport'} 
                 onClick={() => selectSpecialTile('Teleport')}>
                 <FlyingSaucer className='w-12 h-12'/>
             </SpecialTileItem>
+
             <SpecialTileItem 
                 iconColor='green' 
-                title='Spawn Point' 
-                description='Specify where players spawn into your Realm.' 
+                title='Điểm xuất hiện' 
+                description='Xác định nơi người chơi xuất hiện trong thế giới.' 
                 selected={specialTile === 'Spawn'} 
                 onClick={() => selectSpecialTile('Spawn')}>
                 <Tree className='w-12 h-12'/>
